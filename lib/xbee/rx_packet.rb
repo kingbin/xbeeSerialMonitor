0d39a83346252fc419251faa6ee82e84c5439154
Chris Blazek <chris.blazek@gmail.com>
2011-12-07 23:17:15 -0600

    added catch for corrupt rx messages, hoping to at least get the size

--- lib/xbee/rx_packet.rb
+++ lib/xbee/rx_packet.rb
@@ -4,14 +4,16 @@ class RX_Packet
   attr_accessor :msg, :length, :frame_type, :src_addy_64, :src_addy_16, :rcv_op, :rcv_data, :chk_sum
 
   def initialize(*msg)
-    #"Got this NOISE: #{msg.join(',')}"
     self.msg = msg
     self.length = (Integer("0x"+msg[1]) * 255) + Integer("0x"+msg[2])
-    self.frame_type = msg[3];
-    self.src_addy_64 = msg[4..11];
-    self.src_addy_16 = (Integer("0x"+msg[12]) * 255) + Integer("0x"+msg[13])
-    self.rcv_op = msg[14]
-    self.rcv_data = msg[15,self.length-12]
-    self.chk_sum = msg[self.length+3]
+    totallngth = self.length+3
+    if msg.length == (totallngth+1)
+      self.frame_type = msg[3];
+      self.src_addy_64 = msg[4..11];
+      self.src_addy_16 = (Integer("0x"+msg[12]) * 255) + Integer("0x"+msg[13])
+      self.rcv_op = msg[14]
+      self.rcv_data = msg[15,self.length-12]
+      self.chk_sum = msg[totallngth]
+    end
   end
 end
